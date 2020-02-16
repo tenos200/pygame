6d92e903bcfccd4be0843af6497535d2e5fc56c7
tenos200 <tim.enosedu@gmail.com>
2020-02-16 00:09:18 +0000

    first try snake

---
+++ snake.py
@@ -0,0 +1,172 @@
+import tkinter as tk
+import pygame
+import math
+from tkinter import messagebox
+
+class cube(object):
+    rows = 20 
+    w = 500 
+
+    def __init__(self,start, dirnx=1,dirny=0,color=(0,255,0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos(self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    def draw(self, surface, eyes=False):
+
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface, self.color, (i*dis+1, j*dis+1, dis-2, dis-2))
+        if eyes: # Draws the eyes
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+
+    
+    
+class snake(object):
+
+    body = []
+    turns = {}
+
+
+    def __init__(self, color, pos):
+
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1 
+
+
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+#if statement for left key prioritizes that one and elif statements on rest ensures that only one key can be pressed at a time
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1 
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0 
+                    self.dirny = -1 
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0 
+                    self.dirny = 1 
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+
+        for i, x in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            #this else statements handles transition from one side on the screen to the other
+            else:
+                if x.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif x.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0, c.pos[1])
+                elif x.dirny == 1 and c.pos[1] >=  c.rows-1: c.pos = (c.pos[0],0)
+                elif x.dirnx == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows -1)
+                else: c.move(c.dirnx, c.dirny)
+
+
+
+
+
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+    
+    def draw(self, surface):
+
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+
+    for l in range(rows):
+        x = x + sizeBtwn 
+        y = y + sizeBtwn 
+
+        #draws 2 lines for every for loop
+        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
+
+
+def redrawWindow(surface):
+    global rows, width, s
+    #win.fill black screen
+    s.draw(surface)
+    surface.fill((0,0,0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+
+def message_box(subject, content):
+    pass
+
+def main():
+    global rows, width, s
+    height = 500
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s = snake((0,255,0), (10,10))
+    clock = pygame.time.Clock()
+    
+    flag = True
+    
+    
+    while flag:
+        pygame.time.delay(50)
+        #the lower delay goes the faster pace of the game becomes
+        clock.tick(10)
+        #the lower clock tick goes the slower pace of the game becomes
+        redrawWindow(win)
+
+
+
+
+
+
+
+main()
+
+
+
