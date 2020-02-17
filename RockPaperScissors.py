07f1f7c1f6bb227eb1d87ef604c9cafb5d18395f
tenos200 <tim.enosedu@gmail.com>
2020-02-17 22:12:36 +0000

    better but far from good, logic works

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,64 +1,45 @@
-import os
-import sys
+import sys    
+
+
 def play():
-
-    i = 5 
-
-    play = True
-    while play:
-        rock = "rocks"
-        scissors = "sicssors"
-        paper = "paper"
-
-        rock > scissors, paper > rock, scissors > paper 
-        
-        print("Welcome to rock, paper, scissor")
-        while (i > 0):
-            playerOne = int(input("Enter 1 for rock:\nEnter 2 for paper:\nEnter 3 for scissors:\nEnter 4 to quit:\nPlayer1>"))
-            if playerOne == 1:
-                playerOnechoice = rock 
-                break
-            elif playerOne == 2:
-                playerOnechoice = paper 
-                break
-            elif playerOne == 2:
-                playerOnechoice = scissors 
-                break
-            elif playerOne == 4:
-                sys.exit()
-            else:
-                print("I don't understand that enter it again")
-
-        while (i > 0): 
-            playerTwo= int(input("\nEnter 1 for rock:\nEnter 2 for paper:\nEnter 3 for scissors:\nEnter 4 to quit:\nPlayer2>"))
-            if playerTwo == 1:
-               playerTwochoice = rock 
-               break
-            elif playerTwo == 2:
-               playerTwochoice = paper 
-               break
-            elif playerTwo == 3:
-               playerTwochoice = scissors 
-               break
-            elif playerTwo == 4:
-                sys.exit()
-            else:
-                print("I don't understand that enter it again")
-
-
-
-        if playerOnechoice > playerTwochoice:
-           print(f"Player one wins with{playerOnechoice}") 
-        elif playerTwochoice > playerOnechoice:
-           print(f"Player two wins with {playerTwochoice}") 
-        elif playerOnechoice == playerTwochoice:
-            print(f"Draw both players draw {playerOnechoice}")
-
-        
-        
-            
-
     
-     
     
+    while True:
+        usr_1 = input("Welcome to rock, paper, scissor\nIf you would like to quit press q\nElse enter your choice\nPlayer1>")
+        usr_2 = input("Player2>")
+
+        if usr_1 == "rock":
+            if usr_2 == "paper":
+                print(f"Player 2 wins")
+            elif usr_2 =="rock":
+                print("Draw")
+            elif usr_2 == "scissor":
+                print("Player 1 wins")
+
+        elif usr_1 == "paper":
+            if usr_2 == "rock":
+                print("Player 1 wins")
+            elif usr_2 == "paper":
+                print("Draw")
+            elif usr_2 == "scissor":
+                print("Player 2 wins")
+
+        elif usr_1 == "scissor":
+            if usr_2 == "rock":
+                print("Player 2 wins")
+            elif usr_2 == "paper":
+                print("Player 1 wins")
+            elif usr_2 == "scissor":
+                print("Draw")
+        elif usr_1 or usr_2 == "q":
+            break
+        else:
+            print("I don't understand that")
+            play()
+
 play()
+
+
+
+
+
